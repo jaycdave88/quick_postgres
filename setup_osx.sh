6ca84ed5afda4ea1539de4b60132a5a6896c514a
bburns-ddog <benjamin.burns@datadoghq.com>
2018-02-23 10:18:47 -0800

    operations db

--- setup_osx.sh
+++ setup_osx.sh
@@ -2,6 +2,10 @@
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib -y
 psql postgres -c "CREATE USER db_user WITH PASSWORD 'db_pass';"
+psql postgres -c "GRANT USAGE ON SCHEMA public TO db_user;"
 psql postgres -c "CREATE DATABASE sightings;"
-psql postgres sightings < sightings.sql
+psql postgres -c "CREATE DATABASE operations;"
 psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE sightings TO db_user;"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE operations TO db_user;"
+psql sightings < sightings.sql
+psql operations < operations.sql
