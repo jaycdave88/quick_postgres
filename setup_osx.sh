b744244cc929cbe197aee39f9ab68714722f6366
jay.dave <jay.dave@JDatadog.local>
2020-01-27 15:12:50 -0600

    changing to username to datadog

--- setup_osx.sh
+++ setup_osx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib -y
-psql postgres -c "CREATE USER db_user WITH PASSWORD 'db_pass';"
-psql postgres -c "GRANT USAGE ON SCHEMA public TO db_user;"
+psql postgres -c "CREATE USER datadog WITH PASSWORD 'password123!';"
+psql postgres -c "GRANT USAGE ON SCHEMA public TO datadog;"
 psql postgres -c "CREATE DATABASE sightings;"
 psql postgres -c "CREATE DATABASE operations;"
-psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE sightings TO db_user;"
-psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE operations TO db_user;"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE sightings TO datadog;"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE operations TO datadog;"
 psql sightings < sightings.sql
 psql operations < operations.sql
