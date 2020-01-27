cc48e6bac2b06bf78ded28ccba8d59ecfda410f8
jay.dave <jay.dave@JDatadog.local>
2020-01-27 15:15:26 -0600

    updating to datadog user

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # sudo apt-get update
 # sudo apt-get install postgresql postgresql-contrib -y
-sudo -u postgres psql -c "CREATE USER db_user WITH PASSWORD 'db_pass';"
+sudo -u postgres psql -c "CREATE USER datadog WITH PASSWORD 'datadog';"
 sudo -u postgres psql -c "CREATE DATABASE sightings;"
-sudo -u postgres psql -c "GRANT USAGE ON SCHEMA public TO db_user;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sightings TO db_user;"
+sudo -u postgres psql -c "GRANT USAGE ON SCHEMA public TO datadog;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sightings TO datadog;"
 sudo -u postgres psql sightings < sightings.sql
 sudo -u postgres psql -c "CREATE DATABASE operations;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE operations TO db_user;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE operations TO datadog;"
 sudo -u postgres psql operations < operations.sql
